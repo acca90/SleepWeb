a30817d5e3f786cd2421307eb40aa7e2318c1a08
Matheus Hernandes <matheusrher@gmail.com>
2019-07-21 15:47:13 -0300

    Consumo dos grupos para o compartilhamento de dados

--- backend/modules/monitoring/service.py
+++ backend/modules/monitoring/service.py
@@ -6,15 +6,40 @@ Universidade de Passo Fundo - 2018/2019
 @author Matheus Hernandes
 @since 07/07/2019
 """
+from django.db.models import Q
+
+from backend.modules.group.models import Group
 from backend.modules.monitoring.models import Monitoring
+from backend.modules.user.models import User
 
 
 class MonitoringService:
+    """
+    Service to keep complex rules and exposing methods for monitoring
+    """
 
     def store(self, monitorings):
+        """
+        Method defined to store new monitoring records
+        """
         for monitoring in monitorings:
             new_monitoring = Monitoring()
             new_monitoring.copy(monitoring)
 
             if not new_monitoring.exists():
                 new_monitoring.save()
+
+    def filter(self, request):
+        """
+        Method defined to filter monitoring follower user privilege and group restrictions
+        """
+        # if request.user.is_superuser:
+        #     return Monitoring.objects.all()
+
+        user = User.objects.get(pk=request.user.id)
+        shared_patients = Group.objects.filter(Q(users=user) | Q(institutions=user.institution)).values('patients')
+
+        return Monitoring.objects.filter(
+            Q(patient__user=user) |
+            Q(patient__in=shared_patients)
+        )
--- backend/modules/monitoring/views.py
+++ backend/modules/monitoring/views.py
@@ -7,27 +7,33 @@ Universidade de Passo Fundo - 2018/2019
 @since 30/05/2019
 """
 from rest_framework import viewsets
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from backend.modules.monitoring.models import Monitoring
 from backend.modules.monitoring.serializers import MonitoringReadSerializer
+from backend.modules.monitoring.service import MonitoringService
 
 
 class MonitoringReadOnlyViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    A simple ViewSet for viewing accounts.
+    A simple ViewSet for viewing monitorings.
     """
     permission_classes = (IsAuthenticated,)
     queryset = Monitoring.objects.all()
     serializer_class = MonitoringReadSerializer
+    monitoring_service = MonitoringService()
 
     def list(self, request, *args, **kwargs):
         """
         Override method to check permissions
         """
+        self.queryset = self.monitoring_service.filter(request)
         return super().list(request, args, kwargs)
 
     def retrieve(self, request, *args, **kwargs):
         """
         Override method to check permissions
         """
-        return super().retrieve(request, args, kwargs)
+        queryset = self.monitoring_service.filter(request)
+        serializer = MonitoringReadSerializer(queryset, many=False)
+        return Response(serializer.data)
