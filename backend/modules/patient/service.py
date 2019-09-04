da79e65c51d4f2dfa23afbc8f072c3219cc06059
Matheus Hernandes <matheusrher@gmail.com>
2019-09-04 19:45:25 -0300

    Cadastro de pacientes em módulos externos pt2

--- backend/modules/patient/service.py
+++ backend/modules/patient/service.py
@@ -6,9 +6,10 @@ Universidade de Passo Fundo - 2018/2019
 @author Matheus Hernandes
 @since 02/09/2019
 """
-import requests, json
-from rest_framework import status
+import json
+import requests
 from django.core import serializers
+from rest_framework import status
 from backend.modules.institution.models import Institution
 from backend.modules.msystem.models import MSystem
 from backend.modules.patient.models import Patient
@@ -18,6 +19,7 @@ class PatientService:
     """
     Service to keep logics and exposing methods for patients
     """
+
     @staticmethod
     def send(patient_pk):
         """
@@ -25,8 +27,11 @@ class PatientService:
         """
         try:
             # Patient
-            patient = Patient.objects.filter(pk=patient_pk)
-            serialized = serializers.serialize("json", patient)
+            serialized = serializers.serialize(
+                "json",
+                Patient.objects.filter(pk=patient_pk),
+                fields=('uuid', 'first_name', 'last_name', 'birth_date', 'gender')
+            )
 
             # Instances to send patient
             msystems = MSystem.objects.filter(
@@ -35,11 +40,10 @@ class PatientService:
 
             # Send patient for each instance
             for location in msystems:
-                response = requests.post(location.url + 'patient', json=json.loads(serialized)[0].fields)
+                response = requests.post(location.url + 'patient', json=json.loads(serialized)[0]['fields'])
                 if response.status_code != 200:
                     raise Exception(response.status_code)
 
         except Exception as e:
             print(e)
             raise Exception(status.HTTP_500_INTERNAL_SERVER_ERROR)
-
